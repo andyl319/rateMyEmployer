3ed682d627c259f795bc116b430cea6fc5f3ff59
Harrison Bui <hsbui@hotmail.com>
2017-04-30 17:02:22 -0700

    user controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ActiveRecord::Base
+  attr_reader :password
+
   validates :username, :password_digest, :session_token, presence: true
   validates :username, uniqueness: true
   validates :password, length: { minimum: 6 }, allow_nil: :true
 
   after_initialize :ensure_session_token
   before_validation :ensure_session_token_uniqueness
-
-  attr_reader :password
 
   def self.find_by_credentials(username, password)
     user = User.find_by_credentials(username: username)
